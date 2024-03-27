7949cacece06cbf3bfbc31754c59f731d1f4da5b
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-03-27 19:25:08 -0400

    Structured file to prevent unnecessary conflicts

--- qBay.java
+++ qBay.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class qBay {
     public static void main(String[] args) {
-
+        welcomeScreen();
+        mainMenu();
     }
+
+    private static void welcomeScreen() {
+    }
+
+    private static void mainMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Buy, Sell, Cart, Logout");
+        switch (scanner.nextLine()) {
+            case "Buy":
+                buyMenu();
+                break;
+            case "Sell":
+                sellMenu();
+                break;
+            case "Cart":
+                cartMenu();
+                break;
+            case "Logout":
+                logout();
+                break;
+        }
+        scanner.close();
+    }
+
+    private static void buyMenu() {
+    }
+
+    private static void sellMenu() {
+    }
+
+    private static void cartMenu() {
+    }
+
+    private static void logout() {
+    }
+
+
 }
