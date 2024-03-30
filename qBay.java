205fdfd0ad55f6b7d3621b46d7925ff1a51f4110
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-03-29 21:32:42 -0400

    Added welcome message

--- qBay.java
+++ qBay.java
@@ -3,10 +3,20 @@ import java.util.Scanner;
 public class qBay {
     public static void main(String[] args) {
         welcomeScreen();
-        mainMenu();
+        if (login()) mainMenu();
     }
 
     private static void welcomeScreen() {
+        System.out.println(" __      __         .__                                       __                    __________                 \n" +
+                "/  \\    /  \\  ____  |  |    ____    ____    _____    ____   _/  |_   ____     ______\\______   \\_____    ___.__.\n" +
+                "\\   \\/\\/   /_/ __ \\ |  |  _/ ___\\  /  _ \\  /     \\ _/ __ \\  \\   __\\ /  _ \\   / ____/ |    |  _/\\__  \\  <   |  |\n" +
+                " \\        / \\  ___/ |  |__\\  \\___ (  <_> )|  Y Y  \\\\  ___/   |  |  (  <_> ) < <_|  | |    |   \\ / __ \\_ \\___  |\n" +
+                "  \\__/\\  /   \\___  >|____/ \\___  > \\____/ |__|_|  / \\___  >  |__|   \\____/   \\__   | |______  /(____  / / ____|\n" +
+                "       \\/        \\/            \\/               \\/      \\/                      |__|        \\/      \\/  \\/     ");
+    }
+
+    private static boolean login() {
+        return true;
     }
 
     private static void mainMenu() {
